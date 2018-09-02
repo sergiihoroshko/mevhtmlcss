7c23bf0ae09f2f9b8cd0af670743ee463f58b47b
Sergii Horoshko <sergii.horoshko@mev.com>
2018-09-02 17:38:58 +0300

    Added npm and gulp

---
+++ lesson02/gulpfile.js
@@ -0,0 +1,13 @@
+var
+    gulp  = require("gulp"),
+    livereload = require("gulp-livereload");
+
+gulp.task("reload-css", function() {
+    gulp.src('./styles/*.css')
+        .pipe(livereload());
+});
+
+gulp.task("default", function() {
+    livereload.listen();
+    gulp.watch('./styles/*.css', ['reload-css']);
+});﻿
